41e52be5e1e43c2d949e7f0b5eb4bc4619041fad
k0oda <kulevgen32@gmail.com>
2026-09-25 20:16:42 +0600

    enable transient starship prompt

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -23,6 +23,7 @@ if status is-interactive
 
     if type -q starship
         starship init fish | source
+        enable_transience
     end
 
     # -----------------------------------------------------
